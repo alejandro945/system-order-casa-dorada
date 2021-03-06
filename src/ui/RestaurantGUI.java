6f7192e90aba17f9e00d1e08233c5b2b4f504a6c
luis486 <71047563+luis486@users.noreply.github.com>
2021-03-06 15:17:39 -0500

    We continue with the graphical interface, we delete the show image method because we use the image as the background

--- src/ui/RestaurantGUI.java
+++ src/ui/RestaurantGUI.java
@@ -1,6 +1,5 @@
 package ui;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
@@ -18,12 +17,6 @@ public class RestaurantGUI {
     
     public RestaurantGUI(Restaurant restaurant){
         this.restaurant = restaurant;
-    }
-
-    public Image showImage(){
-        Image image = new Image("La-casa-dorada.jpg");
-        imageView.setImage(image);
-        return image;
     }
     
 }
