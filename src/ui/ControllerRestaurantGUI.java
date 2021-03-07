826fd3cc100c0f4a0deef80bff77cbc0e2219a8a
luis486 <71047563+luis486@users.noreply.github.com>
2021-03-07 14:33:05 -0500

    We continue with the GUI, and added more functional requirements

--- src/ui/ControllerRestaurantGUI.java
+++ src/ui/ControllerRestaurantGUI.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
 import model.*;
@@ -16,6 +18,39 @@ public class ControllerRestaurantGUI implements Initializable {
 
     @FXML
     private Pane mainPane;
+
+    @FXML
+    private TableView<Product> listProducts;
+
+    @FXML
+    private TableColumn<Product, Integer> colPriceProducts;
+
+    @FXML
+    private TableColumn<Product, String> colNameProducts;
+
+    @FXML
+    private TableColumn<Product, String> colProductType;
+
+    @FXML
+    private TableColumn<Product, String> colIngredientsProducts;
+
+    @FXML
+    private TableColumn<Product, String> colSizeProducts;
+
+    @FXML
+    private TextField txtNameProducts;
+
+    @FXML
+    private TextField txtProductType;
+
+    @FXML
+    private TextField txtIngredientsProducts;
+
+    @FXML
+    private TextField txtSizeProducts;
+
+    @FXML
+    private TextField txtPriceProducts;
 
     public Restaurant restaurant;
 
@@ -32,6 +67,27 @@ public class ControllerRestaurantGUI implements Initializable {
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
+
+   
+    @FXML
+    void actualizeProducts(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addProducts(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteProducts(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inhabilitProducts(ActionEvent event) {
 
     }
 
