5ecd53e12c20c0852a31d700799cc32c4c03f17f
luis486 <71047563+luis486@users.noreply.github.com>
2021-03-07 15:18:22 -0500

    Compilation and functionality bug fixes for some buttons.

--- src/ui/ControllerRestaurantGUI.java
+++ src/ui/ControllerRestaurantGUI.java
@@ -17,6 +17,24 @@ import model.*;
 public class ControllerRestaurantGUI implements Initializable {
 
     @FXML
+    private PasswordField txtPasswordLogin;
+
+    @FXML
+    private TextField txtNameUserLogin;
+
+    @FXML
+    private PasswordField txtPasswordRegister;
+
+    @FXML
+    private TextField txtNameRegister;
+
+    @FXML
+    private TextField txtLastNameRegister;
+
+    @FXML
+    private TextField txtNameUserRegister;
+
+    @FXML
     private Pane mainPane;
 
     @FXML
@@ -63,6 +81,34 @@ public class ControllerRestaurantGUI implements Initializable {
         fxmlLoader.setController(this);
         Parent login = fxmlLoader.load();
         mainPane.getChildren().setAll(login);
+    }
+
+    @FXML
+    void singUp(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void showRegister(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("register.fxml"));
+        fxmlLoader.setController(this);
+        Parent root = fxmlLoader.load();
+        mainPane.getChildren().clear();
+        mainPane.getChildren().setAll(root);
+    }
+
+    @FXML
+    void createAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showLogIn(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+        fxmlLoader.setController(this);
+        Parent root = fxmlLoader.load();
+        mainPane.getChildren().clear();
+        mainPane.getChildren().setAll(root);
     }
 
     @Override
